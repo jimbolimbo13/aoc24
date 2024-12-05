5b22e00f9949e8328f0f9adcdf6503d3da0046d3
Jim Beatty <jim@lilcloud.dev>
2024-12-05 07:57:09 -0500

    day1 - part2 solution

--- sql/day1/day1.sql
+++ sql/day1/day1.sql
@@ -8,7 +8,14 @@ CREATE TEMP TABLE right_side AS
 select split_part(values, ' ', 4) as point from day1
 order by point;
 
-SELECT SUM( ABS(rpoint::int - lpoint::int) ) as distance
+CREATE TEMP TABLE similarities AS
+SELECT ls.point::int as left_val, COUNT(rs.point)  as right_count
+FROM left_side AS ls
+LEFT JOIN right_side AS rs
+ON ls.point = rs.point
+GROUP BY ls.point;
+
+SELECT SUM( ABS(rpoint::int - lpoint::int) ) as part1_solution
 FROM (
   SELECT row_number() OVER (), point AS lpoint
   from left_side
@@ -18,3 +25,6 @@ INNER JOIN(
   FROM right_side
 ) as rs
   USING (row_number);
+
+SELECT SUM(left_val * right_count) as part2_solution
+FROM similarities;
